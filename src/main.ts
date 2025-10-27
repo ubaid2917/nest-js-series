6e341fa0dbd48245595ba39c107c8cfa4f7c229c
ubaid naeem <ubaid29170@gmail.com>
2025-10-27 19:43:21 +0500

    add api-key-guard

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
-
+import { AuthGuard } from './guards/auth/auth.guard';
+import { ApiKeyGuard } from './guards/auth/api-key.guard';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
@@ -11,7 +12,11 @@ async function bootstrap() {
       whitelist:true,
       forbidNonWhitelisted:true,
     })
-  )
+  );
+  app.useGlobalGuards(
+    new AuthGuard(),
+    new ApiKeyGuard(),
+  );
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
