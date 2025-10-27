abc61f05242c5a69a6c7095cb920fe527388990a
ubaid naeem <ubaid29170@gmail.com>
2025-10-27 19:01:16 +0500

    add custom pipe

--- src/student/student.controller.ts
+++ src/student/student.controller.ts
@@ -1,30 +1,33 @@
-import { Body, Controller, Delete, Get, Param, Post  } from '@nestjs/common';
-import { StudentService } from './student.service'; 
+import { Body, Controller, Delete, Get, Param, Post } from '@nestjs/common';
+import { StudentService } from './student.service';
 import { CreateStudentDto } from './dto/create-student.dto';
-
+import { UppercasePipe } from '../common/pipes/uppercase/uppercase.pipe';
 @Controller('student')
 export class StudentController {
   constructor(private readonly studentService: StudentService) { }
 
-    @Get()
-    getAllStudents() {
-        return this.studentService.getAllStudents();
-    }   
+  @Get()
+  getAllStudents() {
+    return this.studentService.getAllStudents();
+  }
 
-    @Get(':id')
-    getStudentById(@Param('id') id:string){
-      return this.studentService.getStudentById(Number(id));
-    }     
+  @Get(':id')
+  getStudentById(@Param('id') id: string) {
+    return this.studentService.getStudentById(Number(id));
+  }
 
 
-    @Post()
-    createStudent(@Body() createStudentDto: CreateStudentDto) {
-      return this.studentService.createStudent(createStudentDto);
-    }   
+  @Post()
+  createStudent(
+    @Body('name', UppercasePipe) name: string,
+    @Body() createStudentDto: CreateStudentDto) {
+    createStudentDto.name = name;  
+    return this.studentService.createStudent(createStudentDto);
+  }
 
-    @Delete(':id')
-    deleteStudent(@Param('id') id: string) {
-     return this.studentService.deleteStudent(Number(id));
-    }
+  @Delete(':id')
+  deleteStudent(@Param('id') id: string) {
+    return this.studentService.deleteStudent(Number(id));
+  }
 
 }
