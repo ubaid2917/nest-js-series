fed7fdd7ee86ab36a50167bbb1bfc3be74bc9bf6
ubaid naeem <ubaid29170@gmail.com>
2025-10-28 14:40:21 +0500

    apply global exception filter

--- src/main.ts
+++ src/main.ts
@@ -3,23 +3,29 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { AuthGuard } from './guards/auth/auth.guard';
 import { ApiKeyGuard } from './guards/auth/api-key.guard';
+import { HttpExceptionFilter } from './filters/http-exception/http-exception.filter';
+
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);  
+  const app = await NestFactory.create(AppModule);
 
   const reflector = app.get(Reflector);
 
+  // global pipes
   app.useGlobalPipes(
     new ValidationPipe({
-      transform:false,
-      whitelist:true,
-      forbidNonWhitelisted:true,
+      transform: false,
+      whitelist: true,
+      forbidNonWhitelisted: true,
     })
   );
+  // global guards
   app.useGlobalGuards(
     new AuthGuard(reflector),
     new ApiKeyGuard(),
   );
+  // global exception filter 
+  app.useGlobalFilters(new HttpExceptionFilter());
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
